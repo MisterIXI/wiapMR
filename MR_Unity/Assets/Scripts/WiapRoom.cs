ce872d98ceb5a5962f358d4d0c7fe37523eb14e8
Yannik <yannik.braendle@t-online.de>
2022-05-21 16:21:17 +0200

    first steps for wiapRoom.cs

--- MR_Unity/Assets/Scripts/WiapRoom.cs
+++ MR_Unity/Assets/Scripts/WiapRoom.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Realtime;
+using Photon.Pun;
 using UnityEngine;
 
-public class WiapRoom : MonoBehaviour
+public class WiapRoom : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.SendRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        //PhotonNetwork.Instantiate(photonUserPrefab.name, new Vector3(0, 0, 0), Quaternion.identity);
     }
 }
