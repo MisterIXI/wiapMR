9c0931e53d26e4da4f291f3a9065fc674a1bd1b9
Yannik <yannik.braendle@t-online.de>
2022-05-31 01:50:43 +0200

    after merge cleanup to remove erronous files

--- MR_Unity/Assets/GameBoard.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class GameBoard : MonoBehaviour
-{
-    private List<SnapPoint> snapPoints;
-    public List<SnapPoint> SnapPoints { get { return snapPoints; } }
-}
